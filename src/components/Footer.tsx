c2d28ede18fa62ed8c886d45f8d9658305c7d8a3
Ilya Paripsa <iparips@gmail.com>
2019-08-25 18:56:28 +1000

    Comma separate links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ type Props = {
 export const Footer = ({onCompletedClick, onActiveClick, onAllClick}: Props) => {
     return (
         <div>
-            <a href="#" onClick={onAllClick}>All</a>
-            <a href="#" onClick={onCompletedClick}>Completed</a>
+            <a href="#" onClick={onAllClick}>All</a>,
+            <a href="#" onClick={onCompletedClick}>Completed</a>,
             <a href="#" onClick={onActiveClick}>Active</a>
         </div>
     );
